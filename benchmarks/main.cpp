71a1baf7b7a4a9c87153151191ffe2a126d22868
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-02 00:57:44 +0200

    Improves benchmarks

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include "secs/container.h"
 
+// TODO: these benchmarks are too simplisitc be meanigful, improve them!
+
 namespace chrono = std::chrono;
 
 using std::cout;
@@ -23,7 +25,7 @@ struct Velocity {
   {}
 };
 
-static const size_t COUNT = 10000;
+static const size_t COUNT = 100000;
 
 template<typename F>
 void benchmark(const string& label, F&& body) {
@@ -49,8 +51,14 @@ float compute(Velocity& v) {
   return v.x + v.y;
 }
 
+static float sum = 0;
+
+void use(float value) {
+  sum += value;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
-void vector_of_values() {
+void iterate_vector_of_values() {
   vector<Velocity> inputs;
   inputs.reserve(COUNT);
 
@@ -58,17 +66,18 @@ void vector_of_values() {
     inputs.emplace_back(random_number(), random_number());
   }
 
-  vector<float> outputs;
-  outputs.reserve(COUNT);
+  float result = 0;
 
   benchmark("vector of values", [&]() {
     for (auto& input : inputs) {
-      outputs.push_back(compute(input));
+      result += compute(input);
     }
   });
+
+  use(result);
 }
 
-void vector_of_pointers() {
+void iterate_vector_of_pointers() {
   vector<unique_ptr<Velocity>> inputs;
   inputs.reserve(COUNT);
 
@@ -76,17 +85,18 @@ void vector_of_pointers() {
     inputs.push_back(make_unique<Velocity>(random_number(), random_number()));
   }
 
-  vector<float> outputs;
-  outputs.reserve(COUNT);
+  float result = 0;
 
   benchmark("vector of pointers", [&]() {
     for (auto& input : inputs) {
-      outputs.push_back(compute(*input));
+      result += compute(*input);
     }
   });
+
+  use(result);
 }
 
-void container_with_entity() {
+void iterate_container_without_load() {
   Container container;
 
   for (size_t i = 0; i < COUNT; ++i) {
@@ -94,17 +104,18 @@ void container_with_entity() {
     e.create_component<Velocity>(random_number(), random_number());
   }
 
-  vector<float> outputs;
-  outputs.reserve(COUNT);
+  float result = 0;
 
-  benchmark("container iteration via entity", [&]() {
-    for (auto c : container.entities().need<Velocity>()) {
-      outputs.push_back(compute(*c.entity().component<Velocity>()));
+  benchmark("container without load", [&]() {
+    for (auto e : container.entities()) {
+      result += compute(*e.component<Velocity>());
     }
   });
+
+  use(result);
 }
 
-void container_with_cursor() {
+void iterate_container_with_load() {
   Container container;
 
   for (size_t i = 0; i < COUNT; ++i) {
@@ -112,21 +123,60 @@ void container_with_cursor() {
     e.create_component<Velocity>(random_number(), random_number());
   }
 
-  vector<float> outputs;
-  outputs.reserve(COUNT);
+  float result = 0;
 
-  benchmark("container iteration via cursor", [&]() {
-    for (auto c : container.entities().need<Velocity>()) {
-      outputs.push_back(compute(c.get<Velocity>()));
+  benchmark("container with load", [&]() {
+    for (auto e : container.entities().load<Velocity>()) {
+      result += compute(*e.component<Velocity>());
     }
   });
+
+  use(result);
+}
+
+void compare_component_ptr_and_raw_ptr() {
+  Container container;
+
+  for (size_t i = 0; i < COUNT; ++i) {
+    auto e = container.create();
+    e.create_component<Velocity>(random_number(), random_number());
+  }
+
+  std::vector<secs::ComponentPtr<Velocity>> component_ptrs;
+  component_ptrs.reserve(COUNT);
+
+  std::vector<Velocity*> raw_ptrs;
+  raw_ptrs.reserve(COUNT);
+
+  for (auto e : container.entities().need<Velocity>()) {
+    component_ptrs.push_back(e.component<Velocity>());
+    raw_ptrs.push_back(e.component<Velocity>().get());
+  }
+
+  float result = 0;
+
+  benchmark("ComponentPtr<T> access", [&]() {
+    for (auto p : component_ptrs) {
+      result += compute(*p);
+    }
+  });
+
+  benchmark("T* access", [&]() {
+    for (auto p : raw_ptrs) {
+      result += compute(*p);
+    }
+  });
+
+  use(result);
 }
 
 int main() {
-  vector_of_values();
-  vector_of_pointers();
-  container_with_entity();
-  container_with_cursor();
+  iterate_vector_of_values();
+  iterate_vector_of_pointers();
+  iterate_container_without_load();
+  iterate_container_with_load();
+
+  compare_component_ptr_and_raw_ptr();
 
   return 0;
 }
